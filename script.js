093c3cb1b88160eb38916f500885711685916b28
Andrei-Anaheim <86974515+Andrei-Anaheim@users.noreply.github.com>
2025-07-16 14:48:28 +0300

    Update script.js
    upd2

--- script.js
+++ script.js
@@ -304,7 +304,7 @@ function addVisitsClick() {
     document.getElementById('table_field').innerHTML='';
     document.getElementById('visits_list').classList.add('hide');
     document.getElementById('save_edit_visit').classList.add('hide');
-    document.getElementById('instrument').style.display = 'block'
+    setTimeout(()=> {document.getElementById('instrument').style.display = 'block'}, 1000);
 }
 
 document.getElementById('confirm_visit').addEventListener('click',confirmVisitsClick);
